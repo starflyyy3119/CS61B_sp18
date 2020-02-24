41273925a6f165ce5182e9238ae33c1a8a9d76cc
starflyyy3119 <starfly3110@gmail.com>
2020-02-24 10:06:38 +0800

    finish lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList temp = A;
+        while(temp.rest != null) temp = temp.rest;
+        temp.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +94,8 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) return B;
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
