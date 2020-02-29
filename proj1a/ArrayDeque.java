c2a929802fc72c3e3eca889368841bbbf0ead30f
starflyyy3119 <starfly3110@gmail.com>
2020-02-29 12:21:06 +0800

    new version for ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,101 +1,121 @@
 /**
- * Array implementation of deque
+ * ArrayDequeInitial.java has passed the auto grader, but it is somewhat ugly
+ * This implementation gets an inspiration from
+ * @source https://www.cnblogs.com/xzxl/p/8643448.html
  */
 public class ArrayDeque<T> {
-    private int size;
-    /* pointer used to indicate the head of the deque, arrayList[front + 1] is the first item*/
-    private int front;
-    /* pointer used to indicate the last of the deque, arrayList[last - 1] is the last item */
-    private int last;
-    private T[] arrayList;
+    /* array is empty: front == last */
+
+    /* number of items in the array: (last - front) mod array.length */
+    /* a mod b: we try to find some q and r(must be positive), so that a = q * b + r*/
+    /* for example: -6 mod 7, we have -6 = (-1) * 7 + 1, so -6 mod 7 = 1*/
+
+    /* array is full: ((last + 1) mod array.length) == front */
+
+
+    private int front; // array[front] is the first item of the array
+    private int last;  // array[last - 1] is the last item of the array
+    private T[] array;
 
     public ArrayDeque() {
-        size = 0;
         front = 0;
-        last = 1;
-        arrayList = (T[]) new Object[8];
+        last = 0;
+        array = (T[]) new Object[8];
     }
 
-    public void addFirst(T item) {
-        size = size + 1;
-        if (size == arrayList.length) {
-            resize(size * 2);
-        }
-        arrayList[front] = item;
-
-        front = Math.floorMod(front - 1, arrayList.length);
+    /**
+     * A helper function that use to circulate the index
+     * @param index that needs to be circulated
+     * @return the circulated index
+     */
+    private int cast(int index) {
+        return Math.floorMod(index, array.length);
     }
 
-    public void addLast(T item) {
-        size = size + 1;
-        if (size == arrayList.length) {
-            resize(size * 2);
-        }
-        arrayList[last] = item;
-
-        last = (last + 1) % arrayList.length;
-    }
-
-    private void resize(int capacity) {
-        T[] array = (T[]) new Object[capacity];
-        arrayCopy(arrayList, array, front + 1);
-        front = array.length - 1;
-        last = size - 1;
-        arrayList = array;
-    }
-
-    private void arrayCopy(T[] arraySource, T[] arrayDes, int index) {
-        for (int i = 0; i < size; i++) {
-            arrayDes[i] = arraySource[index % arraySource.length];
-            index++;
-        }
+    private boolean isFull() {
+        return (cast(last + 1) == front);
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return front == last;
     }
 
     public int size() {
-        return size;
+        return cast(last - front);
     }
 
-    public void printDeque() {
-        int f = (front + 1) % arrayList.length;
-        for (int cnt = 0; cnt < size; cnt++) {
-            StdOut.print(arrayList[f] + " ");
-            f = (f + 1) % arrayList.length;
+    /**
+     * resize the array, and set front = 0, set last = size
+     */
+    private void resize(int capacity) {
+        if (capacity < 8) return;
+        int size = size();
+        T[] newArray = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i + front];
         }
+        array = newArray;
+        front = 0;
+        last = size;
+    }
+
+    public void addFirst(T item) {
+        if (isFull()) {
+            resize(size() * 2);
+        }
+        if (isEmpty()) {
+            array[front] = item;
+            last = cast(last + 1);
+        }
+        else { // !isEmpty()
+            front = cast(front - 1);
+            array[front] = item;
+        }
+
+    }
+
+    public void addLast(T item) {
+        if (isFull()) {
+            resize(size() * 2);
+        }
+
+        array[last] = item;
+        last = cast(last + 1);
     }
 
     public T removeFirst() {
-        if (size == 0) return null;
-        front = (front + 1) % arrayList.length;
-        T item = arrayList[front];
-        arrayList[front] = null;
-        size--;
-
-        if ((double) size / arrayList.length < 0.25) {
-            resize(arrayList.length / 2);
-            last = last + 1;
+        if (isEmpty()) {
+            return null;
         }
+
+        T item = array[front];
+        array[front] = null;
+        front = cast(front + 1);
+
+        if ((double) size() / array.length < 0.25) {
+            resize(size() / 2);
+        }
+
         return item;
     }
 
     public T removeLast() {
-        if (size == 0) return null;
-        last = Math.floorMod(last - 1, arrayList.length);
-        T item = arrayList[last];
-        arrayList[last] = null;
-        size--;
-
-        if ((double) size / arrayList.length < 0.25) {
-            resize(arrayList.length / 2);
-            last = last + 1;
+        if (isEmpty()) {
+            return null;
         }
+
+        last = cast(last - 1);
+        T item = array[last];
+        array[last] = null;
+
+        if ((double) size() / array.length < 0.25) {
+            resize(size() / 2);
+        }
+
         return item;
     }
 
     public T get(int index) {
-        return arrayList[(front + index + 1) % arrayList.length];
+        return array[cast(index + front)];
     }
 }
---
+++ proj1a/ArrayDequeInitial.java
@@ -0,0 +1,105 @@
+/**
+ * Array implementation of deque
+ */
+public class ArrayDequeInitial<T> {
+    private int size;
+    /* pointer used to indicate the head of the deque, arrayList[front + 1] is the first item*/
+    private int front;
+    /* pointer used to indicate the last of the deque, arrayList[last - 1] is the last item */
+    private int last;
+    private T[] arrayList;
+
+    public ArrayDequeInitial() {
+        size = 0;
+        front = 0;
+        last = 1;
+        arrayList = (T[]) new Object[8];
+    }
+
+    public void addFirst(T item) {
+        size = size + 1;
+        if (size == arrayList.length) {
+            resize(size * 2);
+        }
+        arrayList[front] = item;
+
+        front = Math.floorMod(front - 1, arrayList.length);
+    }
+
+    public void addLast(T item) {
+        size = size + 1;
+        if (size == arrayList.length) {
+            resize(size * 2);
+        }
+        arrayList[last] = item;
+
+        last = (last + 1) % arrayList.length;
+    }
+
+    private void resize(int capacity) {
+        T[] array = (T[]) new Object[capacity];
+        arrayCopy(arrayList, array, front + 1);
+        front = array.length - 1;
+        last = size - 1;
+        arrayList = array;
+    }
+
+    private void arrayCopy(T[] arraySource, T[] arrayDes, int index) {
+        for (int i = 0; i < size; i++) {
+            arrayDes[i] = arraySource[index % arraySource.length];
+            index++;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int f = (front + 1) % arrayList.length;
+        for (int cnt = 0; cnt < size; cnt++) {
+            StdOut.print(arrayList[f] + " ");
+            f = (f + 1) % arrayList.length;
+        }
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        front = (front + 1) % arrayList.length;
+        T item = arrayList[front];
+        arrayList[front] = null;
+        size--;
+
+        if ((double) size / arrayList.length < 0.25) {
+            resize(arrayList.length / 2);
+            last = last + 1;
+        }
+        return item;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        last = Math.floorMod(last - 1, arrayList.length);
+        T item = arrayList[last];
+        arrayList[last] = null;
+        size--;
+
+        if ((double) size / arrayList.length < 0.25) {
+            resize(arrayList.length / 2);
+            last = last + 1;
+        }
+        return item;
+    }
+
+    public T get(int index) {
+        return arrayList[(front + index + 1) % arrayList.length];
+    }
+}
